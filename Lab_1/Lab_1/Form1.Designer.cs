83adb3119e5086824c8ad72de2debfd64dbe5e94
h4rrydabrwn <huanphan2497@gmail.com>
2025-03-14 14:40:31 +0700

    Task 5

--- Lab_1/Lab_1/Form1.Designer.cs
+++ Lab_1/Lab_1/Form1.Designer.cs
@@ -32,14 +32,15 @@
             bBai2 = new Button();
             bBai3 = new Button();
             bBai4 = new Button();
+            bBai5 = new Button();
             SuspendLayout();
             // 
             // bBai1
             // 
-            bBai1.Location = new Point(34, 42);
-            bBai1.Margin = new Padding(2, 1, 2, 1);
+            bBai1.Location = new Point(68, 101);
+            bBai1.Margin = new Padding(4, 2, 4, 2);
             bBai1.Name = "bBai1";
-            bBai1.Size = new Size(75, 31);
+            bBai1.Size = new Size(150, 74);
             bBai1.TabIndex = 0;
             bBai1.Text = "Bai 1";
             bBai1.UseVisualStyleBackColor = true;
@@ -47,10 +48,10 @@
             // 
             // bBai2
             // 
-            bBai2.Location = new Point(220, 42);
-            bBai2.Margin = new Padding(2, 1, 2, 1);
+            bBai2.Location = new Point(440, 101);
+            bBai2.Margin = new Padding(4, 2, 4, 2);
             bBai2.Name = "bBai2";
-            bBai2.Size = new Size(75, 31);
+            bBai2.Size = new Size(150, 74);
             bBai2.TabIndex = 1;
             bBai2.Text = "Bài 2";
             bBai2.UseVisualStyleBackColor = true;
@@ -58,10 +59,10 @@
             // 
             // bBai3
             // 
-            bBai3.Location = new Point(34, 87);
-            bBai3.Margin = new Padding(2, 1, 2, 1);
+            bBai3.Location = new Point(68, 209);
+            bBai3.Margin = new Padding(4, 2, 4, 2);
             bBai3.Name = "bBai3";
-            bBai3.Size = new Size(75, 27);
+            bBai3.Size = new Size(150, 65);
             bBai3.TabIndex = 2;
             bBai3.Text = "Bài 3";
             bBai3.UseVisualStyleBackColor = true;
@@ -69,25 +70,36 @@
             // 
             // bBai4
             // 
-            bBai4.Location = new Point(220, 87);
-            bBai4.Margin = new Padding(2, 1, 2, 1);
+            bBai4.Location = new Point(440, 209);
+            bBai4.Margin = new Padding(4, 2, 4, 2);
             bBai4.Name = "bBai4";
-            bBai4.Size = new Size(75, 27);
+            bBai4.Size = new Size(150, 65);
             bBai4.TabIndex = 3;
             bBai4.Text = "Bài 4";
             bBai4.UseVisualStyleBackColor = true;
             bBai4.Click += bBai4_Click;
             // 
+            // bBai5
+            // 
+            bBai5.Location = new Point(68, 326);
+            bBai5.Name = "bBai5";
+            bBai5.Size = new Size(150, 46);
+            bBai5.TabIndex = 4;
+            bBai5.Text = "Bài 5";
+            bBai5.UseVisualStyleBackColor = true;
+            bBai5.Click += bBai5_Click;
+            // 
             // Form1
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleDimensions = new SizeF(14F, 36F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(400, 188);
+            ClientSize = new Size(800, 451);
+            Controls.Add(bBai5);
             Controls.Add(bBai4);
             Controls.Add(bBai3);
             Controls.Add(bBai2);
             Controls.Add(bBai1);
-            Margin = new Padding(2, 1, 2, 1);
+            Margin = new Padding(4, 2, 4, 2);
             Name = "Form1";
             Text = "Lab_1";
             ResumeLayout(false);
@@ -99,5 +111,6 @@
         private Button bBai2;
         private Button bBai3;
         private Button bBai4;
+        private Button bBai5;
     }
 }
---
+++ Lab_1/Lab_1/Lab1_bai5.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab_1
+{
+    public partial class Lab1_bai5 : Form
+    {
+        public Lab1_bai5()
+        {
+            InitializeComponent();
+        }
+
+        private void Lab1_bai5_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private string XepLoai(List<double> diemList, double diemTB)
+        {
+
+            double diemThapNhat = diemList.Min();
+            if (diemTB >= 8 && diemThapNhat >= 6.5) return "Gioi";
+            else if (diemTB >= 6.5 && diemThapNhat >= 5) return "Kha";
+            else if (diemTB >= 5 && diemThapNhat >= 3.5) return "TB";
+            else if (diemTB >= 3.5 && diemThapNhat >= 2) return "Yeu";
+            else return "Kem";
+        }
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void bXuat_Click(object sender, EventArgs e)
+        {
+            gbDiem.Visible = true;
+            string input = tbInput.Text;
+            string[] diemString = input.Split(',');
+            
+            List<double> diemList = new List<double>();
+
+            int soMonPass = 0;
+
+            for(int i = 0; i < diemString.Length; ++i)
+            {
+                if (double.TryParse(diemString[i].Trim(), out double diem))
+                {
+
+                    diemList.Add(diem);
+                    if (i % 4 == 0 && i != 0)
+                    {
+                        tbDisplay.AppendText($"\nMôn {i + 1}: {diem}\t");
+                    }
+                    else
+                    {
+                        tbDisplay.AppendText($"Môn {i + 1}: {diem}\t");
+                    }
+                    if (diem >= 5) ++soMonPass;
+                }
+            }
+
+            double diemTB = diemList.Average();
+            double diemCaoNhat = diemList.Max();
+            double diemThapNhat = diemList.Min();
+            int soMonFail = diemString.Length - soMonPass;
+            string HocLuc = XepLoai(diemList, diemTB);
+
+            tbDiemTB.Text = Convert.ToString(diemTB);
+            tbDiemCaoNhat.Text = Convert.ToString(diemCaoNhat);
+            tbSoMonPass.Text = Convert.ToString(soMonPass);
+            tbDiemThapNhat.Text = Convert.ToString(diemThapNhat);
+            tbXepLoai.Text = Convert.ToString(HocLuc);
+            tbSoMonFail.Text = Convert.ToString(soMonFail);
+            
+        }
+    }
+}
