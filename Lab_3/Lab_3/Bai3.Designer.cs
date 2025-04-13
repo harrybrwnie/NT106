cb1a60ae9f8e36b5bf5bf27a220a2f3a6a5d8f14
kha <cube71kd@gmail.com>
2025-04-13 14:04:58 +0700

    bai 2 + 3 + remove bin and obj

--- Lab_3/Lab_3/Bai3.Designer.cs
+++ Lab_3/Lab_3/Bai3.Designer.cs
@@ -28,18 +28,48 @@
         /// </summary>
         private void InitializeComponent()
         {
+            TCPServerbtn = new Button();
+            TCPClientbtn = new Button();
             SuspendLayout();
+            // 
+            // TCPServerbtn
+            // 
+            TCPServerbtn.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            TCPServerbtn.Location = new Point(57, 28);
+            TCPServerbtn.Name = "TCPServerbtn";
+            TCPServerbtn.Size = new Size(363, 52);
+            TCPServerbtn.TabIndex = 0;
+            TCPServerbtn.Text = "Open TCP Server";
+            TCPServerbtn.UseVisualStyleBackColor = true;
+            TCPServerbtn.Click += TCPServerbtn_Click;
+            // 
+            // TCPClientbtn
+            // 
+            TCPClientbtn.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            TCPClientbtn.Location = new Point(57, 86);
+            TCPClientbtn.Name = "TCPClientbtn";
+            TCPClientbtn.Size = new Size(363, 52);
+            TCPClientbtn.TabIndex = 1;
+            TCPClientbtn.Text = "Open TCP Client";
+            TCPClientbtn.UseVisualStyleBackColor = true;
+            TCPClientbtn.Click += TCPClientbtn_Click;
             // 
             // Bai3
             // 
-            AutoScaleDimensions = new SizeF(14F, 36F);
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(1356, 793);
+            ClientSize = new Size(465, 166);
+            Controls.Add(TCPClientbtn);
+            Controls.Add(TCPServerbtn);
+            Margin = new Padding(2, 1, 2, 1);
             Name = "Bai3";
             Text = "Bai3";
             ResumeLayout(false);
         }
 
         #endregion
+
+        private Button TCPServerbtn;
+        private Button TCPClientbtn;
     }
 }
